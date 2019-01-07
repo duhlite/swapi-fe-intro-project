210d440d3b3620fe7f3042a00cdd95cc1c807ed5
Steve Romain <smromain@gmail.com>
2019-01-06 19:19:58 -0600

    Flesh out app.js a bit more.

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
 // using node, create a simple webserver here that displays a static page
+// that static page will live in your views folder
+
+// you may want to have an index.html and a page.html (or something similar)
+// or even do your API calls on the server side rather than on the client
+
+// feel free to use as little or as much backend as you would like here
